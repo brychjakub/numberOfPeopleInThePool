7fb83c76059e3d432924283839b3b0ec4f30643a
Jakub Brych <96384633+brychjakub@users.noreply.github.com>
2023-01-15 10:25:41 +0100

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -1,6 +1,9 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 from datetime import datetime, date
+import pandas as pd
+
 
 today = date.today()
 date = today.strftime("%d/%m/%Y")
@@ -19,13 +22,48 @@ soup = BeautifulSoup(res.text, "html.parser")
 found = soup.select(".field__item")[3]
 
 #exact <p>tag with my info
-pTag = found.find_all("p")[-1]
+pTag = str(found.find_all("p")[-1])
 
 #extracting the exact info 
-numberOfPeopleInThePool = (str(pTag)[22:24])
+numberOfPeopleInThePool = (re.findall(r'\d+', pTag))[0]
 print(numberOfPeopleInThePool)
 
+"""
+if day == "Monday":
+        monday_data = [['Time', 'Number of people'],
+                    ['09:00', 20],
+                    ['10:00', 25],
+                    ['11:00', 30]]
+
+if day == "Tuesday":
+    tuesday_data = [['Time', 'Number of people'],
+                ['09:00', 15],
+                ['10:00', 20],
+                ['11:00', 25]]
+
+if day == "Wednesday":
+    wednesday_data = [['Time', 'Number of people'],
+                  ['09:00', 10],
+                  ['10:00', 15],
+                  ['11:00', 20]]
+sunday_data = [current_time, numberOfPeopleInThePool]
 
 
+
+df1 = pd.DataFrame(monday_data, columns=['Time','Number of people'])
+df1.insert(0,'Day', 'Monday')
+
+df2 = pd.DataFrame(tuesday_data, columns=['Time','Number of people'])
+df2.insert(0,'Day', 'Tuesday')
+
+df3 = pd.DataFrame(wednesday_data, columns=['Time','Number of people'])
+df3.insert(0,'Day', 'Wednesday')
+
+df4 = pd.DataFrame(sunday_data, columns=[current_time, numberOfPeopleInThePool])
+
+result = pd.concat([df4])
+
+result.to_csv('data.csv', index=False, mode="a")
+"""
 
 
