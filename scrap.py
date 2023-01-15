34d0a5b8d2552abb08b8cb3f266013f2c7d90947
brychjakub <96384633+brychjakub@users.noreply.github.com>
2023-01-15 20:49:27 +0100

    ů

--- scrap.py
+++ scrap.py
@@ -6,39 +6,37 @@ import pandas as pd
 import schedule
 import time
 
-
-today = date.today()
-date = today.strftime("%d/%m/%Y")
-day = today.strftime("%A")
-print(day,date)
-
-now = datetime.now()
-
-current_time = now.strftime("%H:%M:%S")
-print(current_time)
-
-res = requests.get("https://www.kravihora-brno.cz/kryta-plavecka-hala")
-soup = BeautifulSoup(res.text, "html.parser")
-
-#HTML class where is the info I need
-found = soup.select(".field__item")[3]
-
-#exact <p>tag with my info
-pTag = str(found.find_all("p")[-1])
-
-#extracting the exact info 
-numberOfPeopleInThePool = (re.findall(r'\d+', pTag))[0]
-print(numberOfPeopleInThePool)
-
-
 def intoCsv():
+            today = date.today()
+            date_today = today.strftime("%d/%m/%Y")
+            day = today.strftime("%A")
+            print(day)
+
+            now = datetime.now()
+
+            current_time = now.strftime("%H:%M:%S")
+            print(current_time)
+
+            res = requests.get("https://www.kravihora-brno.cz/kryta-plavecka-hala")
+            soup = BeautifulSoup(res.text, "html.parser")
+
+            #HTML class where is the info I need
+            found = soup.select(".field__item")[3]
+
+            #exact <p>tag with my info
+            pTag = str(found.find_all("p")[-1])
+
+            #extracting the exact info 
+            numberOfPeopleInThePool = (re.findall(r'\d+', pTag))[0]
+            print(numberOfPeopleInThePool)
+
+
             day_data = [[current_time, numberOfPeopleInThePool]]
             df1 = pd.DataFrame(day_data, columns=['Time', 'Number of people'])
             result = pd.concat([df1])
             result.to_csv(f'{day}.csv', index=False, mode="a")
 
-schedule.every(5).minutes.do
-schedule.every(5).minutes.do(intoCsv)
+schedule.every(15).seconds.do(intoCsv)
 #schedule.every().hour.do(job)
 #schedule.every().day.at("10:30").do(job)
 
