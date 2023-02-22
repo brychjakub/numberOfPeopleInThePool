9d1cf288b18b3a295e6df79e340a7ee6d92ebcec
Jakub Brych <96384633+brychjakub@users.noreply.github.com>
2023-02-22 10:31:37 +0100

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,31 +1,60 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import csv
 
-# Read the CSV file into a pandas DataFrame and remove every odd row
-data = pd.read_csv("Friday.csv")
-data = data.iloc[::2, :]
 
-# Convert the "Time" column to numerical values
-data["Time"] = pd.to_numeric(data["Time"])
+# necessary to display the y axis in the correct order
+def numbers(csv_file):
+    with open(csv_file, 'r') as f:
+            reader = csv.reader(f)
+            your_list = list(reader)
 
-# Remove all values from "Time" column that are less than or equal to 5 and more than or equal to 22
-data = data[(data["Time"] > 5) & (data["Time"] < 22)]
+    for i in range(len(your_list)):
+            for j in range(len(your_list[i])):
+                if len(your_list[i][j]) == 1:
+                    your_list[i][j] = "00" + your_list[i][j]
+                elif len(your_list[i][j]) == 2:
+                    your_list[i][j] = "0" + your_list[i][j]
 
-# Remove all rows with 0 in the "Number of people" column
-data = data[data["Number of people"] != 0]
+    with open(csv_file, 'w') as f:
+            writer = csv.writer(f)
+            writer.writerows(your_list)
 
-# Sort the DataFrame based on the "Number of people" column
-data = data.sort_values(by=["Number of people"])
 
-# Assign a unique rank to each value in the "Number of people" column, UPDATE: commented out as useless for now
-#data["Rank"] = data["Number of people"].rank(method="dense")
+    # Read the CSV file into a pandas DataFrame and remove every odd row
+def graph(csv_file):
+    data = pd.read_csv(csv_file)
+    data = data.iloc[::2, :]
 
-# Plot the bar chart with sorted y-axis values
-plt.bar(data["Time"], data['Number of people'])
-plt.xlabel('Time')
-plt.ylabel('Number of People')
-plt.title('Number of people in the pool over time')
-plt.show()
+    # Convert the "Time" column to numerical values
+    data["Time"] = pd.to_numeric(data["Time"])
 
-# Save the plot to a file
-plt.savefig("your_graph.png")
+    # Remove all values from "Time" column that are less than or equal to 5 and more than or equal to 22
+    data = data[(data["Time"] > 5) & (data["Time"] < 22)]
+
+    # Remove all rows with 0 in the "Number of people" column
+    data = data[data["Number of people"] != 0]
+
+    # Sort the DataFrame based on the "Number of people" column
+    data = data.sort_values(by=["Number of people"], ascending=True)
+
+    # Plot the bar chart with sorted y-axis values
+    plt.bar(data["Time"], data['Number of people'])
+    plt.xlabel('Time')
+    plt.ylabel('Number of People')
+    plt.title('Number of people in the pool over time')
+    plt.show()
+
+
+def call(csv_file):
+    numbers(csv_file)
+    graph(csv_file)
+
+    
+call("Sunday.csv")
+
+    
+
+
+
+
